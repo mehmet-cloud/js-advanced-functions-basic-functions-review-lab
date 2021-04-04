046f45a96e77c117465495d0c72e976270502ffc
mehmet-cloud <mehmetbozdag155@gmail.com>
2021-04-04 01:10:02 -0400

    Completed until wrapadjective

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+function saturdayFun (activity){
+    if ( activity === undefined){
+     return "This Saturday, I want to roller-skate!"
+    }
+    else{
+    return `This Saturday, I want to ${activity}!`}
+    }
+
+
+function mondayWork (job){
+    if (job === undefined){
+        return "This Monday, I will go to the office."
+    }
+    else{
+        return `This Monday, I will ${job}.`
+    }
+}
+    
+
+function wrapAdjective (){
+return }
+
+  
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
